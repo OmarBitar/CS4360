08145e4fd254774a3c0a919ed43fa399ad5d2fca
Justin Wu <justinandrewwu@gmail.com>
2020-09-28 14:04:11 -0600

    Change to basic setup

--- Schedule/db/migrate/20200918193100_add_question_to_users.rb
+++
@@ -1,6 +0,0 @@
-class AddQuestionToUsers < ActiveRecord::Migration[6.0]
-  def change
-    add_column :users, :question, :string, default: ""
-    add_column :users, :answer, :string, default: Base64.encode64(Digest::SHA256.digest(""))
-  end
-end
