57ea891b613b310dfb9a12c160d7b25dab2892d4
Omar Bitar <obitar@yahoo.com>
2020-10-16 14:42:17 -0600

    Heroku hosting (#12)
    * heroko ready
    
    * removing unecessary files
    
    * removing more unecessary files
    
    * removing even more unecessary files
    
    * bring back pacakge.json file
    
    * fixed .yml formatting
    
    * again fixed .yml formatting
    
    * possible fix in dockerfile
    
    * revert docker change
    
    * host config for heroku
    
    * configured puma.rb based on heroku docs
    
    * removing PIDFILE from puma.rb as possible fix to herok hosting
    
    * code cleaning for puma.rb
    
    * .env was added to .gitignore
    
    * Delete .env
    
    this file shouldn't have been pushed in the first place, .env will be added to .gitignore will be

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -4,6 +4,11 @@ Rails.application.configure do
   # Code is not reloaded between requests.
   config.cache_classes = true
 
+  # docker yarn fix
+  config.webpacker.check_yarn_integrity = false
+  # heroku fix
+  config.hosts << ENV['POSTGRES_HOST']
+
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
   # and those relying on copy on write to perform better.
