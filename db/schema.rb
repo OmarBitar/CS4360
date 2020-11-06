25eb131be56a52b015e430240494c966799b88f3
Omar Bitar <obitar@yahoo.com>
2020-11-05 18:41:39 -0700

    devise now works with sqlite3

--- db/schema.rb
+++ db/schema.rb
@@ -53,7 +53,6 @@ ActiveRecord::Schema.define(version: 2020_10_29_174128) do
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
     t.string "unconfirmed_email"
-    t.integer "failed_attempts", default: 0, null: false
     t.string "unlock_token"
     t.datetime "locked_at"
     t.datetime "created_at", precision: 6, null: false
