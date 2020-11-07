1c2be9ad318ccc8266538be5721f6edd78bbb9b5
Omar Bitar <obitar@yahoo.com>
2020-11-07 11:15:53 -0700

    added controler and views for custom devise regestration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_29_174128) do
+ActiveRecord::Schema.define(version: 2020_11_07_180239) do
 
   create_table "coverage", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
@@ -57,6 +57,7 @@ ActiveRecord::Schema.define(version: 2020_10_29_174128) do
     t.datetime "locked_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "company_name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
