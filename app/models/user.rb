efd4c81a8658d6746c64f0971eb1523bf23dbdc9
Omar Bitar <obitar@yahoo.com>
2020-11-07 17:44:46 -0700

    improved validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,22 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :company_name, presence: true, length: { minimum: 2, maximum: 15 }
+  validate :password_requirements_are_met
+
+  # password_requirement code is referenced
+  # from: https://stackoverflow.com/a/50334086/11379938
+  def password_requirements_are_met
+    rules = {
+        " must contain at least one lowercase letter"  => /[a-z]+/,
+        " must contain at least one uppercase letter"  => /[A-Z]+/,
+        " must contain at least one digit"             => /\d+/,
+        " must contain at least one special character" => /[^A-Za-z0-9]+/
+    }
+
+    rules.each do |message, regex|
+      errors.add( :password, message ) unless password.match( regex )
+    end
+  end
 end
