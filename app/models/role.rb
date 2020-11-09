75961a8429a546eb7b24379ea7a0e173ee764255
Omar Bitar <obitar@yahoo.com>
2020-11-09 16:21:18 -0700

    Revert "Merge branch 'roles' of github.com:nweicker/CS4360 into roles"
    This reverts commit a3071c047905765feef99f8e6375ec971f4c7beb, reversing
    changes made to 608c83e6d3b4af680e21d32ad56091eca0a6d7fe.

---
+++ app/models/role.rb
@@ -0,0 +1,6 @@
+class Role < ApplicationRecord
+  has_and_belongs_to_many :employees, :optional => true
+
+  validates :name, uniqueness: true, presence: true, length: { maximum: 15 }
+  validates :priority, uniqueness: true, presence: true, numericality: { only_integer: true, :greater_than_or_equal_to => 1 }
+end
