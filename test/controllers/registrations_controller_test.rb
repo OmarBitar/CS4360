17f49c951c3a6525c363e2db01ab5d3da1b25a4a
Justin Wu <justinandrewwu@gmail.com>
2020-11-20 12:54:36 -0700

    Update registrations_controller_test.rb

--- test/controllers/registrations_controller_test.rb
+++ test/controllers/registrations_controller_test.rb
@@ -7,4 +7,30 @@ class RegistrationsControllerTest < ActionDispatch::IntegrationTest
         assert_response :redirect
     end
 
+    test "Login Screen" do
+        get "/users/sign_in"
+        assert_response :success
+    end
+
+    test "Sign up" do
+        get "/users/sign_up"
+        assert_response :success
+    end
+
+    #Views test
+    test "Email field" do
+        get "/users/sign_in"
+        assert_select "input#user_email", 1
+    end
+
+    test "Password field" do
+        get "/users/sign_in"
+        assert_select "input#user_password", 1
+    end
+
+    test "Sign up link" do
+        get "/users/sign_in"
+        assert_select "a", {count: 1, text: "Sign up"}
+    end
+
 end
