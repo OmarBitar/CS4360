d258504e7df5603bb5396814e5868dfdca095aa1
Nicole W <29169568+nweicker@users.noreply.github.com>
2020-11-20 17:46:32 -0700

    Update application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,21 +3,4 @@ class ApplicationController < ActionController::Base
   def after_sign_in_path_for(resource)
     root_path
   end
-
-  def times_to_array(params)
-    val = '['
-    # variable location: models/concerns/filter.rb
-    $days.each do |day|
-      start = params[:employee][:times]["#{day}"][:start]
-      stop = params[:employee][:times]["#{day}"][:end]
-
-      unless start == '' and stop == ''
-        val.length > 1 ? val += ', ' : ''
-        val += "#{day} [#{start}, #{stop}]"
-        day == $days.last ? val += ']' : ''
-      end
-    end
-    val
-  end
-
 end
