c1850fc1d6fe18ac4af917a64e4269339725a877
Omar Bitar <obitar@yahoo.com>
2020-11-24 16:26:30 -0700

    Revert "Revert "Merge branch 'master' into coveragePage""
    This reverts commit f055cc29b53b26db310dc6e111d27e7614eb3faf.

--- app/controllers/roles_controller.rb
+++ app/controllers/roles_controller.rb
@@ -4,7 +4,7 @@ class RolesController < ApplicationController
   # GET /roles
   # GET /roles.json
   def index
-    @roles = Role.by_user(current_user)
+    @roles = Role.user(current_user).order(:priority).search(params[:search])
   end
 
   # GET /roles/1
@@ -15,16 +15,21 @@ class RolesController < ApplicationController
   # GET /roles/new
   def new
     @role = Role.new
+    @employees = Employee.user(current_user)
+    @roles = Role.user(current_user)
+    @next_priority = @roles.length == 0 ? 1 : @roles.maximum(:priority) + 1
   end
 
   # GET /roles/1/edit
   def edit
+    @employees = Employee.user(current_user)
   end
 
   # POST /roles
   # POST /roles.json
   def create
     @role = Role.new(role_params)
+    @employees = Employee.user(current_user)
 
     respond_to do |format|
       if @role.save
@@ -70,6 +75,7 @@ class RolesController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def role_params
-      params.require(:role).permit(:user_id, :name, :priority)
+      params.require(:role).permit(:user_id ,:name, :priority,:first_name, :last_name, :active, :availability, :employee_ids => [])
+
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,17 +2,11 @@ Rails.application.routes.draw do
   devise_for :users, controllers: { 
       registrations: 'users/registrations'
   }
-
   root "welcome#index"
-
-  get "coverage", to: "coverage#index"
   get "/users/edit", to: "users#edit"
 
   resources :schedules
-  resources :coverage
   resources :coverage_rules
   resources :roles
   resources :employees
-
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
