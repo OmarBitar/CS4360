6cdf0d3e91c85f7d960decc7e598b89c33a3ad63
Nicole W <nweicker@msudenver.edu>
2020-11-30 17:49:20 -0700

    fixes to last pull request

--- app/javascript/fullcalendar.js
+++ app/javascript/fullcalendar.js
@@ -1,7 +1,58 @@
 import { Calendar} from '@fullcalendar/core';
-
 import interactionPlugin from '@fullcalendar/interaction';
 import timeGridPlugin from "@fullcalendar/timegrid";
+
+
+document.addEventListener('turbolinks:load', function() {
+    let calendarEl = document.getElementById('calendar');
+
+    let calendar = new Calendar(calendarEl, {
+        plugins: [ interactionPlugin, timeGridPlugin ],
+        events: gon.events,
+        timeZone: false,
+        initialView: 'timeGridWeek',
+        selectable: true,
+        selectMirror: true,
+        selectMinDistance: 1,
+        allDaySlot: false,
+        displayEventTime: true,
+        headerToolbar: {
+            start:'prev',
+            center:'title',
+            end:'next',
+        },
+        footerToolbar: {
+            start: 'addShift',
+        },
+
+        // Passes the formatted start and end times to the new shifts form
+        select: function(selection) {
+            if (selection.start.getUTCDate() === (selection.end.getUTCDate())) {
+                // newShift tells the form to override the default values
+                sessionStorage.setItem("newShift", true);
+                sessionStorage.setItem("date", format(selection.start, "date"));
+                sessionStorage.setItem("start", format(selection.start, "time"));
+                sessionStorage.setItem("end", format(selection.end, "time"));
+                window.open(gon.new_shift, "_self");
+            }
+            else {
+                alert("Overnight shifts are not supported.");
+                calendar.unselect();
+            }
+        },
+
+        customButtons: {
+            addShift: {
+                text: 'Add Shift',
+                click: function () {
+                    window.open(gon.new_shift,"_self");
+                },
+            },
+        },
+    });
+    calendar.render();
+});
+
 
 // takes a time object and returns a string of HH:MM or YYYY-MM-DD
 function format (time, type) {
@@ -24,48 +75,3 @@ function format (time, type) {
             return year + "-" + month + "-" + day;
     }
 }
-
-document.addEventListener('turbolinks:load', function() {
-    var calendarEl = document.getElementById('calendar');
-
-    var calendar = new Calendar(calendarEl, {
-        plugins: [ timeGridPlugin, interactionPlugin ],
-        events: gon.events,
-        timeZone: false,
-        selectable: true,
-        selectMirror: true,
-        selectMinDistance: 1,
-        editable: true,
-        allDaySlot: false,
-        displayEventTime: false,
-        headerToolbar: {
-            start:'prev',
-            center:'title',
-            end:'next',
-        },
-        footerToolbar: {
-            start: 'custom1',
-        },
-
-        // Passes the formatted start and end times to the new shifts form
-        select: function(selectionInfo) {
-            // newShift tells the form to override the default values
-            sessionStorage.setItem("newShift", true);
-            sessionStorage.setItem("date", format(selectionInfo.start, "date"));
-            sessionStorage.setItem("start", format(selectionInfo.start, "time"));
-            sessionStorage.setItem("end", format(selectionInfo.end, "time"));
-            window.open(gon.new_shift, "_self");
-        },
-
-        customButtons: {
-            custom1: {
-                text: 'Add Shift',
-                click: function () {
-                    window.open(gon.new_shift_path,"_self");
-                },
-            },
-        },
-    });
-
-    calendar.render();
-});
--- app/models/role.rb
+++ app/models/role.rb
@@ -2,9 +2,9 @@ class Role < ApplicationRecord
   include Filter
 
   has_and_belongs_to_many :employees, optional: true
-  has_many :shifts, dependent: :nullify
+  has_many :shifts, dependent: :destroy
 
-  validates :name, uniqueness: {scope: :user_id}, presence: true
+  validates :name, uniqueness: {scope: :user_id, case_sensitive: false}, presence: true
   validates :priority, numericality: {only_integer: true, greater_than_or_equal_to: 1 }
 
 end
